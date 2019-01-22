75e3ac6c4b429366b641beddfb1fd36a496d7de1
trevor78 <36665098+trevor78@users.noreply.github.com>
2019-01-22 11:58:34 +0530

    Update shellTest.sh

--- shellTest.sh
+++ shellTest.sh
@@ -1,17 +1,17 @@
-case $space in
-2*|4*)
-  Message="All is quiet."
-  echo $Message
-   Message="Start thinking about cleaning out some stuff.  There's a partition that is $space % full."
-  ;;
-3*)
-  echo $Message
-  ;;
-6*)
-  echo $Message
-  ;;
-*)
-  echo "default"
-  ;;
-esac
-echo "hiii"
+. shellTest.cnf
+
+echo "whse batch started"
+#select rows from database with the given condition by establishing a connection with the db using userid and password
+echo "SELECT item_nbr, movement_status, storage_location FROM Received_Items where movement_status = 'pending'" | mysql $database -h $HOST -u $userid -p $password > $filename
+
+if [ -f $filename ]
+then
+#setting host of ftp
+echo "initiating file transfer"
+scp $filename $USER@$HOST:/downloads/
+
+else
+        echo "no files to transfer"
+fi
+
+echo "whse batch completed"
